f82e2deb2f48695270b263f7d61d2eb0e824c552
Aditya Bajpai <bajpaiaditya356@gmail.com>
2021-05-15 01:28:16 +0530

    praw user auth changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 import praw
 from urllib.request import urlretrieve
 from pprint import PrettyPrinter
 import apod_object_parser
 import os
-from config import api_key , password , client_secret , client_id
+from config import api_key, password, client_secret, client_id
 import requests
 app = Flask(__name__)
 pp = PrettyPrinter()
@@ -13,35 +13,34 @@ pp = PrettyPrinter()
 response = apod_object_parser.get_data(api_key)
 
 
-
-
 picture = apod_object_parser.get_url(response)
 
 reddit = praw.Reddit(client_id=client_id,
-                    client_secret=client_secret,
-                    username="Adi_who_doesnt_simp",
-                    password=password,
-                    user_agent="prawpraw",
-                    check_for_async=False)
+                     client_secret=client_secret,
+                     username="Adi_who_doesnt_simp",
+                     password=password,
+                     user_agent="prawpraw",
+                     check_for_async=False)
 
 subreddit = reddit.subreddit('aww')
-hot  = subreddit.hot(limit = 50)
-
-
-
+hot = subreddit.hot(limit=50)
 
 
 title = apod_object_parser.get_title(response)
+
+
 @app.route('/')
 def main():
-    return render_template('main.html' , hot = hot)
+    return render_template('main.html', hot=hot)
+
 
 @app.route('/nasa')
 def nasa():
-    return render_template('nasa.html' , picture=picture,title=title)
+    return render_template('nasa.html', picture=picture, title=title)
 
 
-print(picture)
+print("are we good?")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
